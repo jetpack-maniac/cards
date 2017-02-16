e8617ff6f092e5995befd6ebaa0b8b89560c9bae
rtf671 <rtf671@gmail.com>
2017-02-15 21:22:12 -0500

    Added simple cut mechanic and function to print cards out.

--- cards.js
+++ cards.js
@@ -1,6 +1,12 @@
 var suits = ['Diamonds','Clubs','Hearts','Spades']
 var values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
 
+////////////////////////////////////////////////////////////////////////////////
+// Classes
+
+// Deck Class
+
+
 function Deck(){
   this.cards = []
 
@@ -8,10 +14,25 @@ function Deck(){
     for(var value = 0; value < values.length; value++){
       var card = new Card(values[value], suits[suit])
       this.cards.push(card)
-      console.log(card.toString())
     }
   }
 }
+
+Deck.prototype.cut = function(){
+  var packet = this.cards.slice(0,26)
+  var packet2 = this.cards.slice(26)
+  this.cards = packet2.concat(packet)
+}
+
+Deck.prototype.print = function(){
+    for(var i = 0; i < this.cards.length; i++){
+      console.log(this.cards[i].toString())
+    }
+    console.log(this.cards.length + ' cards in the deck')
+
+}
+
+// Card Class
 
 function Card(value, suit){
   this.value = value
@@ -22,4 +43,11 @@ Card.prototype.toString = function(){
   return this.value + " of " + this.suit
 }
 
+// End of Classes
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 var deck = new Deck()
+deck.cut()
+deck.print()
