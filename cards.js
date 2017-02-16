d83cece6cccd40345e60971c3f003b1f49451af7
rtf671 <rtf671@gmail.com>
2017-02-15 20:43:20 -0500

    Initial commit after creating deck of cards.

--- cards.js
+++ cards.js
@@ -1,1 +1,20 @@
-alert("cards!!!");
+var suits = ['Diamonds','Clubs','Hearts','Spades']
+var values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace']
+var deck = []
+
+function Card(value, suit){
+  this.value = value
+  this.suit = suit
+}
+
+Card.prototype.toString = function(){
+  return this.value + " of " + this.suit
+}
+
+for(var suit = 0; suit < suits.length; suit++){
+  for(var value = 0; value < values.length; value++){
+    var card = new Card(values[value], suits[suit])
+    deck.push(card)
+    console.log(card.toString())
+  }
+}
