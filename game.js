10ec0faecd50f5cf69422e1bfcfc483c4f55bc53
rtf671 <rtf671@gmail.com>
2017-02-22 14:20:24 -0500

    Basic web interface prints out the cards dealt.

--- game.js
+++ game.js
@@ -5,8 +5,29 @@ var people = [p1,p2,p3]
 
 var game = new Blackjack(people)
 
+// function parseHand(hand){
+//   if(!hand) return
+//   var cards = ''
+//   for(i in hand){
+//     var cards = cards + hand[i].unicode()
+//   }
+//   return cards
+// }
+
 $(function(){
   var gamename = $('h1.gamename').text(game.name)
+  var players = $('.players')
+  for(i in game.players){
+    var player = $('<div>').text(game.players[i].name)
+    var hand = $('<div>')
+    for(j in game.players[i].hand){
+      var suit = _.lowerCase(suits[game.players[i].hand[j].suit])
+      var card = $('<span>').addClass(suit).html(game.players[i].hand[j].unicode())
+      hand.append(card)
+    }
+    player.append(hand)
+    players.append(player)
+  }
 })
 
 game.round()
