1e73383d9e1e2453e21f43ace8f20713bf101753
rtf671 <rtf671@gmail.com>
2017-02-23 18:16:01 -0500

    Refactor.

--- game.js
+++ game.js
@@ -21,23 +21,24 @@ function clearButtons(){
   $('div.generic').text('')
 }
 
+function formatCard(card){
+  var suit = _.lowerCase(suits[card.suit])
+  return $('<span>').addClass(suit).html(card.unicode())
+}
+
 function drawPlayers(player){
   var playerDiv = $('<div class="player">').text(player.name)
   var handDiv = $('<div class="hand">')
   var scoreDiv = $('<div class="score">')
-  for(j in player.hand){
-    var suit = _.lowerCase(suits[player.hand[j].suit])
-    var card = $('<span>').addClass(suit).html(player.hand[j].unicode())
-    handDiv.append(card)
-  }
+  _.forEach(player.hand, function(card){
+    handDiv.append(formatCard(card))
+  })
   if(player.splitHand.length > 0){
     var splitScoreDiv = $('<div class="splitScore">')
     var splitHandDiv = $('<div class="splitHand">')
-    for(j in player.splitHand){
-      var suit = _.lowerCase(suits[player.splitHand[j].suit])
-      var card = $('<span>').addClass(suit).html(player.splitHand[j].unicode())
-      splitHandDiv.append(card)
-    }
+    _.forEach(player.splitHand, function(card){
+      splitHandDiv.append(formatCard(card))
+    })
     splitScoreDiv.append('Score: ' + game.score(player.splitHand))
     splitHandDiv.append(splitScore)
     playerDiv.append(splitHand)
@@ -45,13 +46,8 @@ function drawPlayers(player){
   scoreDiv.append('Score: ' + game.score(player.hand))
   handDiv.append(scoreDiv)
   playerDiv.append(handDiv)
-  // playersDiv.append(playerDiv)
   if(player != _.last(game.players)) playerDiv.append('<hr>')
   return playerDiv
-}
-
-function drawDealer(player){
-
 }
 
 function drawGame(){
@@ -61,7 +57,6 @@ function drawGame(){
   var playersDiv = $('.players')
   $(function(){
     var newRound = $('<button>').text('New Round').on('click', function(){
-      // $('div.players').text('')
       game.gameloop()
       drawGame()
     })
