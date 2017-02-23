4f2182f7be14fcb378b45b17fb36c8a1b56f0c23
rtf671 <rtf671@gmail.com>
2017-02-23 13:00:54 -0500

    Added first layer of interface.

--- game.js
+++ game.js
@@ -1,40 +1,64 @@
-var p1 = new Player('Tom')
-var p2 = new Player('Dick')
-var p3 = new Player('Harry')
-var people = [p1,p2,p3]
+// var p1 = new Player('Tom')
+// var p2 = new Player('Dick')
+// var p3 = new Player('Harry')
+// var people = [p1,p2,p3]
+var player = new Player('Player')
+var players = [player]
 
-var game = new Blackjack(people)
+
+var game
 
 $(function(){
-  var gamename = $('h1.gamename').text(game.name)
-  var players = $('.players')
-  for(var i in game.players){
-    var player = $('<div class="player">').text(game.players[i].name)
-    var hand = $('<div class="hand">')
-    var score = $('<div class="score">')
-    for(j in game.players[i].hand){
-      var suit = _.lowerCase(suits[game.players[i].hand[j].suit])
-      var card = $('<span>').addClass(suit).html(game.players[i].hand[j].unicode())
-      hand.append(card)
-    }
-    if(game.players[i].splitHand.length > 0){
-      var splitScore = $('<div class="splitScore">')
-      var splitHand = $('<div class="splitHand">')
-      for(j in game.players[i].splitHand){
-        var suit = _.lowerCase(suits[game.players[i].splitHand[j].suit])
-        var card = $('<span>').addClass(suit).html(game.players[i].splitHand[j].unicode())
-        splitHand.append(card)
-      }
-      splitScore.append('Score: ' + game.score(game.players[i].splitHand))
-      splitHand.append(splitScore)
-      player.append(splitHand)
-    }
-    score.append('Score: ' + game.score(game.players[i].hand))
-    hand.append(score)
-    player.append(hand)
-    players.append(player)
-    if(game.players[i] != _.last(game.players)) players.append('<hr>')
-  }
+  var buttons = $('<button>').text('Blackjack').on('click', function(){
+    $('div.players').text('')
+    game = new Blackjack(players)
+    game.gameloop()
+    drawGame()
+  })
+  buttons.appendTo($('div.generic'))
 })
 
-game.gameloop()
+function clearButtons(){
+  $('div.generic').text('')
+}
+
+function drawGame(){
+  clearButtons()
+  $(function(){
+    var buttons = $('<button>').text('New Round').on('click', function(){
+      $('div.players').text('')
+      game.gameloop()
+      drawGame()
+    })
+    buttons.appendTo($('div.generic'))
+    var gamename = $('h1.gamename').text(game.name)
+    var players = $('.players')
+    for(var i in game.players){
+      var player = $('<div class="player">').text(game.players[i].name)
+      var hand = $('<div class="hand">')
+      var score = $('<div class="score">')
+      for(j in game.players[i].hand){
+        var suit = _.lowerCase(suits[game.players[i].hand[j].suit])
+        var card = $('<span>').addClass(suit).html(game.players[i].hand[j].unicode())
+        hand.append(card)
+      }
+      if(game.players[i].splitHand.length > 0){
+        var splitScore = $('<div class="splitScore">')
+        var splitHand = $('<div class="splitHand">')
+        for(j in game.players[i].splitHand){
+          var suit = _.lowerCase(suits[game.players[i].splitHand[j].suit])
+          var card = $('<span>').addClass(suit).html(game.players[i].splitHand[j].unicode())
+          splitHand.append(card)
+        }
+        splitScore.append('Score: ' + game.score(game.players[i].splitHand))
+        splitHand.append(splitScore)
+        player.append(splitHand)
+      }
+      score.append('Score: ' + game.score(game.players[i].hand))
+      hand.append(score)
+      player.append(hand)
+      players.append(player)
+      if(game.players[i] != _.last(game.players)) players.append('<hr>')
+    }
+  })
+}
